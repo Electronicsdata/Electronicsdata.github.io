fdc38b622e4ec2aa36733a3a52c2c0bd9a26a872
Electronicsdata <77414724+Electronicsdata@users.noreply.github.com>
2021-01-20 20:12:20 +0200

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./foundstyle.css";
-import Thefoundation from "./Thefoundation";
- function App() {
-  return (
-    <Thefoundation />
-  );
-}
-export default App
